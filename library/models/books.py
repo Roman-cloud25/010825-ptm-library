f4e3a0cbcb4d7ed89908ea36190f03b28726157a
vladislaav <parieiko.vlad@gmail.com>
2026-02-27 16:16:07 +0400

    Create file `books` for `Book` model

---
+++ library/models/books.py
@@ -0,0 +1,9 @@
+from datetime import date
+
+from django.db import models
+
+
+class Book(models.Model):
+    name:str = models.CharField(max_length=100, verbose_name="Название книги")
+    author: 'Author' = models.ForeignKey(to='Author', on_delete=models.SET_NULL, null=True, related_name='books')
+    published_date:date = models.DateField(verbose_name="Дата публикации")
